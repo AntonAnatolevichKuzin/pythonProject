bdb300889e4fe6039eab3b790376ce2e8a3ff915
Anton Kuzin <kupets1812@gmail.com>
2023-04-30 15:55:06 +0500

    Created class Note

---
+++ Note.py
@@ -0,0 +1,11 @@
+
+class Note:
+    def __init__(self, note_id, title, body):
+        self.id = note_id
+        self.title = title
+        self.body = body
+        self.created_time = datetime.datetime.now()
+        self.last_modified_time = self.created_time
+
+    def __str__(self):
+        return f"ID: {self.id}\nTitle: {self.title}\nBody: {self.body}\nCreated: {self.created_time}\nLast Modified: {self.last_modified_time}"
